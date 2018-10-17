9151ecf4e9584ea0b95ed0d9f888512c9e690c73
Apoorv <apoorvagni@hotmail.com>
2018-10-17 22:59:07 +0530

    bug in ui from vi

--- src/demo.py
+++ src/demo.py
@@ -1,9 +1,9 @@
-import src
 import numpy as np
 import cv2 as cv
-
-
 import sys
+import src
+from sklearn.model_selection import train_test_split
+
 
 # Variables
 ##########################################################
@@ -12,38 +12,66 @@ result_dir = '../result/'
 subjects = range(1, 41)
 subject_images = range(1,11)
 M = len(subject_images) * len(subjects) # total no of images
-K = int(M / 2) # dimention of face_space
+K = int(M / 3) # dimention of face_space
 
 
 # Reading data
 ##########################################################
-# data -> person -> pose (one of the pics)
+# X -> [pose1, pose2, ..., poses]
+# y -> [class, class, ..., class]
 print("Reading data")
-data = {}
+X = []
+y = []
 for subject in subjects:
-	data[subject] = {}
 	for subject_image in subject_images:
 		i_path = path +'s'+ str(subject) + \
-		         '/' + str(subject_image) + '.pgm'
+				'/' + str(subject_image) + '.pgm'
 		temp = cv.imread(i_path)
-		data[subject][subject_image] = temp
+		temp = cv.cvtColor(temp, cv.COLOR_BGR2GRAY) # B&W
+		X.append(temp)
+		y.append(subject)
 del temp
 print ('done')
+# img = np.array(X_train[0])
+# print (img)
+# print (img.dtype)
+# print (img.shape)
+
+
+# Split Train-Test
+##########################################################
+X_train, X_test, y_train, y_test = train_test_split(
+								X, y, random_state=42)
 
 
 # Train the classfier
 ##########################################################
 e = src.EigenFaces(K)
+e.train(X_train, y_train)
 
-print(type(data[1][1])) 
-print(data[1][1].shape)
+print(e.predict(X_train))
+# print(type(data[1][1])) 
+# print(data[1][1].shape)
 
 sys.exit()
 
+# e = src.EigenFaces(2)
 
-i1 = np.array(i1)
-i2 = np.array(i2)
-i3 = np.array(i3)
+# i1 = [[[1, 1, 1], [2, 2, 2], [4, 4, 4]],
+# 	[[23, 23, 23],[23, 23, 23],[53, 53, 53]],
+# 	[[23, 23, 23],[2, 2, 2],[1, 1, 1]]]
 
-e.train(np.array([i1, i2, i3]), np.array([1, 1, 3]))
-e.predict(i3+1, 33, 12)
+# i2 = [[[222, 222, 222], [0, 0, 0], [4, 4, 4]],
+# 	[[33, 33, 33],[24, 24, 24],[3, 3, 3]],
+# 	[[2, 2, 2],[9, 9, 9],[0, 0, 0]]]
+
+# i3 = [[[2, 2, 2], [0, 0, 0], [4, 4, 4]],
+# 	[[3, 3, 3],[4, 4, 4],[3, 3, 3]],
+# 	[[2, 2, 2],[9, 9, 9],[10, 10, 10]]]
+
+# i1 = np.array(i1)
+# i2 = np.array(i2)
+# i3 = np.array(i3)
+
+# e.train(np.array([i1, i2, i3]), np.array([1, 1, 3]))
+# e.predict(i3+1, 33, 12)
--- src/src.py
+++ src/src.py
@@ -29,16 +29,14 @@ class EigenFaces(object):
 		for future recognition.
 		
 	params:
-		X_train: numpy array of 2d array(images)
+		X_train: numpy array of 2d array(images) (greyscale only)
 		Y_train: numpy array of label
 	'''
 	def train(self, X_train, Y_train):
-		self.X_train = X_train
-		self.Y_train = Y_train
 		K = self.K
-		# print ('M = ',len(X_train))
 		M = len(X_train)
 
+		# assume same height and width
 		height, width = X_train[0].shape
 		r = np.zeros((height*width, M))
 
@@ -46,35 +44,37 @@ class EigenFaces(object):
 		for i in range(M):
 			r[:, i] = X_train[i].flatten()
 		Nsq = r.shape[0] # length of one vec
+		assert height*width == Nsq, "bad length of img vctr"
 
-		# print ('flattened train', r)
-		# find the mean image, images stored as rows
-		self.avgImg = r.mean(1)
-		assert len(self.avgImg) == Nsq, "Length of the mean formed bad"
-		# print (self.avgImg)
+		# find the mean image
+		self.avgImg = r.mean(1).reshape((Nsq,1)) # avg across rows
 
 		# find the offsets
-		A = r - self.avgImg.reshape((Nsq,1))
-		#the offsets are stored as cols for each img
-		# print (A)
-		L = np.matmul(A.T, A)
-		# print(L)
-		vals, vi = eigh(L)
-		
-		uvals = np.zeros(K)
-		ui = np.zeros((Nsq, K))
-		# we choose only top K
-		for i in range(K):
-			ui[:,i] = np.matmul(A, vi[M-i-1])
-			uvals[i] = vals[M-i-1]
+		A = r - self.avgImg
+		L = np.matmul(A.T, A) # MxM cov. matrix
+		vals, vi = eigh(L) # since L is PSD
+
+		# we want bigger values first
+		vals = np.flip(vals)
+		vi = np.flip(vi, axis=1)
+
+		# keeping only top K eigenvectors
+		vals = vals[:K]
+		vi = vi[:, :K]
+
+		# getting ui and corresp uvals
+		uvals = vals
+		ui = np.matmul(A, vi)
+
+		assert (ui[:,0] == np.matmul(A,vi[:,0])).all(), 'not equal' #random check :P
 
 		del vals, vi
-		print ('final eigs', ui)
+		# print ('final eigs', ui)
 		# print ('final eigvals', uvals)
 		# normlize
 		for i in range(K):
 			ui[:,i] = ui[:,i] / norm(ui[:,i])
-		print ('normalized eigs', ui)
+		# print ('normalized eigs', ui)
 		# we got the top K ui eigenvectors.
 
 		# will store weigth vectors as cols
@@ -95,53 +95,72 @@ class EigenFaces(object):
 		u, self.indices = np.unique(Y_train, return_index=True)
 		# print (self.indices)
 
-	def predict(self, x, tf, te):
+	'''
+	params:
+		@param te
+		 euclidian threshold used to classify 
+		 images as unkown-face
+		@param tf
+		 euclidian threshold used to classify 
+		 images as non-face
+		@param unknownface
+		 index to return if unkown-face detected
+		@param nonface
+		 index to return if non-face detected
+	'''
+	def predict(self, X, te=2, tf=100000, unknownface=-1, nonface=-2):
 		K = self.K
 		Wei = self.Wei
 		ui = self.ui
+		y = []
 
-		# reshaping the image and finding offset
-		h,w = x.shape
-		Nsq = h*w
-		xr = x.reshape((Nsq, 1))
-		offset = xr - self.avgImg.reshape((Nsq,1))
-		# print (xr, x, offset)
-		# sys.exit()
+		for x in X:
+			# reshaping the image and finding offset
+			h,w = x.shape
+			Nsq = h*w
+			xr = x.reshape((Nsq, 1))
+			offset = xr - self.avgImg.reshape((Nsq,1))
+			# print (xr, x, offset)
+			# sys.exit()
 
-		# finding the weight vecotr of test img
-		weiVct = np.zeros((K, 1))
-		for j in range(K):
-			weiVct[j,0] = np.matmul(offset.T, ui[:, j])
-		# print (weiVct)
+			# finding the weight vecotr of test img
+			weiVct = np.zeros((K, 1))
+			for j in range(K):
+				weiVct[j,0] = np.matmul(offset.T, ui[:, j])
+			# print (weiVct)
 
-		# comparing with other class representatives
-		dist_bw_faces = -1
-		indx = -1
-		for i in self.indices:
-			df = norm(Wei[:, i] - weiVct)
-			if (dist_bw_faces == -1):
-				dist_bw_faces = df
-			else:
-				if (dist_bw_faces != min(dist_bw_faces, df)):
+			# comparing with other class representatives
+			dist_bw_faces = -1
+			indx = -1
+			for i in self.indices:
+				df = norm(Wei[:, i] - weiVct)
+				if (dist_bw_faces == -1):
 					dist_bw_faces = df
-					indx = i
+				else:
+					if (dist_bw_faces != min(dist_bw_faces, df)):
+						dist_bw_faces = df
+						indx = i
 
-		print('indx',indx, '\n wiegths:', Wei, '\nweight', weiVct)
-		# sys.exit()
+			# print('indx',indx, '\n wiegths:', Wei, '\nweight', weiVct)
+			# sys.exit()
 
-		recon = np.zeros((Nsq, 1))
-		for i in range(K):
-			recon += weiVct[i,0]*ui[:,i].reshape((Nsq, 1))
-		print (recon)
+			recon = np.zeros((Nsq, 1))
+			for i in range(K):
+				recon += weiVct[i,0]*ui[:,i].reshape((Nsq, 1))
+			# print (recon)
 
-		dist_bw_faceSpace = norm(recon)
+			dist_bw_faceSpace = norm(recon)
 
-		if dist_bw_faceSpace <= tf and dist_bw_faces <= te:
-			print("yes it is a face, indx", indx)
-		elif dist_bw_faceSpace <= tf and dist_bw_faces > te:
-			print("Unknown Face.")
-		else: # dist_bw_faceSpace > tf
-			print("Non-Face")
+			if dist_bw_faceSpace <= tf and dist_bw_faces <= te:
+				y.append(indx)
+			elif dist_bw_faceSpace <= tf and dist_bw_faces > te:
+				print(dist_bw_faces)
+				y.append(unknownface)
+			else: # dist_bw_faceSpace > tf
+				print(dist_bw_faceSpace)
+				y.append(nonface)
+
+		return y
 
 # find the nearest neigbour of the with the given weights
 # test img provided
